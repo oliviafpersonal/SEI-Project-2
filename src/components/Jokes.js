134e4d643ca51370fd9ab290e87dbaefab3f1d7a
Awyssa <michael.ehson@gmail.com>
2021-03-11 19:05:21 +0000

    update

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -6,52 +6,12 @@ const Home = () => {
   const [joke, setJoke] = useState([])
 
   useEffect(() => {
-    randomJoke()
+    getJoke('any')
   }, [])
 
-  const randomJoke = () => {
+  const getJoke = (category) => {
     const getData = async () => {
-      const response = await axios.get('https://v2.jokeapi.dev/joke/Any?safe-mode')
-      setJoke(response.data)
-    }
-    getData()
-  }
-
-  const spookyJoke = () => {
-    const getData = async () => {
-      const response = await axios.get('https://v2.jokeapi.dev/joke/spooky')
-      setJoke(response.data)
-    }
-    getData()
-  }
-
-  const miscJoke = () => {
-    const getData = async () => {
-      const response = await axios.get('https://v2.jokeapi.dev/joke/misc')
-      setJoke(response.data)
-    }
-    getData()
-  }
-
-  const programmingJoke = () => {
-    const getData = async () => {
-      const response = await axios.get('https://v2.jokeapi.dev/joke/programming')
-      setJoke(response.data)
-    }
-    getData()
-  }
-
-  const christmasJoke = () => {
-    const getData = async () => {
-      const response = await axios.get('https://v2.jokeapi.dev/joke/christmas')
-      setJoke(response.data)
-    }
-    getData()
-  }
-
-  const punJoke = () => {
-    const getData = async () => {
-      const response = await axios.get('https://v2.jokeapi.dev/joke/pun')
+      const response = await axios.get(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
       setJoke(response.data)
     }
     getData()
@@ -62,12 +22,12 @@ const Home = () => {
       <div className="container">
         <div className="categoryDiv">
           <p className="jokeText">Choose a category:</p>
-          <button className="button is-focused" onClick={() => randomJoke()}>Random Joke</button>
-          <button className="button is-primary is-focused" onClick={() => spookyJoke()}>Spooky Joke</button>
-          <button className="button is-link is-focused" onClick={() => miscJoke()}>Other Joke</button>
-          <button className="button is-info is-focused" onClick={() => programmingJoke()}>Programming Joke</button>
-          <button className="button is-success is-focused " onClick={() => christmasJoke()}>Christmas Joke</button>
-          <button className="button is-warning is-focused " onClick={() => punJoke()}>Pun Joke</button>
+          <button className="button is-focused" onClick={() => getJoke('any')}>Random Joke</button>
+          <button className="button is-primary is-focused" onClick={() => getJoke('spooky')}>Spooky Joke</button>
+          <button className="button is-link is-focused" onClick={() => getJoke('misc')}>Other Joke</button>
+          <button className="button is-info is-focused" onClick={() => getJoke('programming')}>Programming Joke</button>
+          <button className="button is-success is-focused " onClick={() => getJoke('christmas')}>Christmas Joke</button>
+          <button className="button is-warning is-focused " onClick={() => getJoke('pun')}>Pun Joke</button>
         </div>
         <h1 className="title is-1 has-text-centered">
           {(!joke.joke) ?
