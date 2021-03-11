bfb988387067f5ca22c7bf18fe993b8d864ba179
Awyssa <michael.ehson@gmail.com>
2021-03-11 12:14:36 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// set up routing for the website
 
 function App() {
-  return <h1>Hello World</h1>
-  <h1> michael </h1>
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/jokes/Random">
+          <Random />
+        </Route>
+        <Route exact path="/jokes/Misc">
+          <Misc />
+        </Route>
+        <Route path="/jokes/Programming">
+          <Programing />
+        </Route>
+        <Route path="/jokes/Pun">
+          <Pun />
+        </Route>
+        <Route path="/jokes/Spooky">
+          <Spooky />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+
 }
 
 export default App
