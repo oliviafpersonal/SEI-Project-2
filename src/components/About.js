2639a691580dd398a6a36b9535220f74a010d6f0
oliviaf <olivia.flynn2@hotmail.com>
2021-03-12 12:07:10 +0000

    more css

--- src/components/About.js
+++ src/components/About.js
@@ -9,17 +9,23 @@ const Home = () => {
         <div className='person'>
           <h1> Olivia Flynn</h1>
           <div className='info'>
-            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQF6x3r13kR2Rg/profile-displayphoto-shrink_200_200/0/1615162371959?e=1620864000&v=beta&t=R1LTPi3GqqCzFrhhZx4TOWdWjPjyJuQcjU4wCGzmkFU" alt="olivia-img" />
+            <div className="photo-container">
+              <span className="btn"><img src="https://media-exp1.licdn.com/dms/image/C4D03AQF6x3r13kR2Rg/profile-displayphoto-shrink_200_200/0/1615162371959?e=1620864000&v=beta&t=R1LTPi3GqqCzFrhhZx4TOWdWjPjyJuQcjU4wCGzmkFU" alt="olivia-img" /></span>
+            </div>
+            <br />
             <p>Software Engineer from London.</p>
-            <p>Likes: Prosecco, Kate Bush, Adventure Time, and long walks in forests.</p>
-            <p>Fun Fact: Color blind but can solve a Rubiks Cube.</p>
+            <p>Likes: Aperol Spritzs in beer gardens, travelling and  .</p>
+            <p>Fun Fact: Has been to 24 countries.</p>
             <p><a href='https://www.linkedin.com/in/olivia-flynn-061518150/' target="blank">LinkedIn</a> | <a href='https://github.com/oliviafpersonal' target='blank'>GitHub</a></p>
           </div>
         </div>
         <div className='person'>
           <h1> Michael Henderson</h1>
           <div className='info'>
-            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGMEdWdBetqYg/profile-displayphoto-shrink_200_200/0/1613654052738?e=1620864000&v=beta&t=A1slZ-XvNW9tTMhe7cZOtMf2j-Iti8hPeLm0rupa2l0" alt="michael-img" />
+            <div className="photo-container">
+              <span className="btn"><img src="https://media-exp1.licdn.com/dms/image/C4D03AQGMEdWdBetqYg/profile-displayphoto-shrink_200_200/0/1613654052738?e=1620864000&v=beta&t=A1slZ-XvNW9tTMhe7cZOtMf2j-Iti8hPeLm0rupa2l0" alt="michael-img" /></span>
+            </div>
+            <br />
             <p>Software Engineer from London.</p>
             <p>Likes: Prosecco, Kate Bush, Sun Ra, and long walks in forests.</p>
             <p>Fun Fact: Color blind but can solve a Rubiks Cube.</p>
