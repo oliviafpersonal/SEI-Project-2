90719ce22b958211e75d9ef98b96fa094435a18e
Awyssa <michael.ehson@gmail.com>
2021-03-12 09:57:47 +0000

    updated rated joke

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = () => {
           <div className="navbar-brand">
             <Link to="/" className="navbar-item">Home</Link>
             <Link to="/Jokes" className="navbar-item">Jokes!</Link>
+            <Link to="/RatedJokes" className="navbar-item">Rated Jokes</Link>
           </div>
         </div>
       </nav>
